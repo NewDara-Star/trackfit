0cbf00a9e517d12b552b8425281680a0cd7eecc5
NewDara-Star <139634597+NewDara-Star@users.noreply.github.com>
2025-01-19 21:21:51 +0000

    Dependencies installed

---
+++ context/AuthContext.tsx
@@ -0,0 +1,61 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { Session, SupabaseClient } from "@supabase/supabase-js";
+import { supabase } from "../lib/supabaseClient";
+
+interface AuthContextType {
+  user: Session | null;
+  loading: boolean;
+  supabase: SupabaseClient;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<Session | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const {
+          data: { session },
+          error,
+        } = await supabase.auth.getSession();
+        if (error) console.error("Error fetching session:", error.message);
+        setUser(session);
+      } catch (error) {
+        console.error("Unexpected error:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getUser();
+
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (_event, session) => {
+        setUser(session);
+      }
+    );
+
+    return () => {
+      authListener?.unsubscribe();
+    };
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading, supabase }}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
+
+export default AuthContext;
